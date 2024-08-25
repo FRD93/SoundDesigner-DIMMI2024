c3cd8f409b5d5511a0768abd746fafed50c1a4be
Francesco Roberto Dani <francesco.dani@supsi.ch>
2024-08-25 21:12:38 +0200

    changed requirements.txt to fit python3.11

--- run.sh
+++ run.sh
@@ -5,9 +5,6 @@ echo "SoundDesigner - ©2024, Francesco Roberto Dani"
 
 # # Linux Installation Process
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        if [[ ! -e "venv" ]]; then
-                python3.11 -m venv venv
-        fi
         source venv/bin/activate
         python graphics.py
 fi
